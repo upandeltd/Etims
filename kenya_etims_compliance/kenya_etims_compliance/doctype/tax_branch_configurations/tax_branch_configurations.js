c617b3172e24a8842f8901d3a05fd0692f5c31cc
kndaniel123 <kndanielkariuki@gmail.com>
2025-02-11 21:36:40 +0300

    add tax branch configs doctype and make chnages on etims code information doctype

--- kenya_etims_compliance/kenya_etims_compliance/doctype/tax_branch_configurations/tax_branch_configurations.js
+++ kenya_etims_compliance/kenya_etims_compliance/doctype/tax_branch_configurations/tax_branch_configurations.js
@@ -3,18 +3,27 @@
 
 frappe.ui.form.on("Tax Branch Configurations", {
     onload(frm){
-        if(!frm.doc.tax_accounts && frm.doc.tax_accounts.length == 0){
+        if(!frm.doc.tax_accounts){
             // Loop through accounts and add them to the child table
             accounts.forEach(account => {
                 let child = frm.add_child("tax_accounts");
                 child.account_name = account.account_name;
                 child.tax_code = account.tax_code;
+                child.account_type = account.account_type;
                 child.tax_rate = account.tax_rate;
             });
 
             // Refresh the field to reflect changes
             frm.refresh_field("tax_accounts");
         }
+
+        frm.set_query("parent_tax_account", function() {
+            return {
+                "filters": {
+                    "company": frm.doc.company
+                }
+            };
+        });
     },
     parent_tax_account(frm){
         if(frm.doc.tax_accounts){
@@ -26,6 +35,20 @@ frappe.ui.form.on("Tax Branch Configurations", {
                 frm.refresh_field("tax_accounts");
             }
         }    
+    },
+    update_stock_selling(frm){
+        if(frm.doc.update_stock_selling == 1){
+            frappe.msgprint("If this action is enabled, Sales Invoices with update stock option enabled will also send the stock movement and stock master data to eTIMS.")
+        }else{
+            frappe.msgprint("If this action is disabled, Sales Invoices with update stock option enabled will not send the stock movement and stock master data to eTIMS.")
+        }
+    },
+    update_stock_purchase(frm){
+        if(frm.doc.update_stock_purchase == 1){
+            frappe.msgprint("If this action is enabled, Purchase Invoices with update stock option enabled will also send the stock movement and stock master data to eTIMS.")
+        }else{
+            frappe.msgprint("If this action is disabled, Purchase Invoices with update stock option enabled will not send the stock movement and stock master data to eTIMS.")
+        }
     },
     create_accounts(frm) {
         if(!frm.doc.__islocal){
@@ -44,7 +67,6 @@ frappe.ui.form.on("Tax Branch Configurations", {
                         callback: function (r) {
                             if(r.message){
                                 if(r.message.status == "Success"){
-                                    frm.doc.accounts_created = 1
 
                                     frm.refresh_field("accounts_created")
                                 }
@@ -67,6 +89,7 @@ frappe.ui.form.on("Tax Branch Configurations", {
                 let child = frm.add_child("item_tax_configuration");
                 child.title = tax_template.title;
                 child.code = tax_template.code;
+                child.code_name = tax_template.code_name;
                 child.tax_rate = tax_template.tax_rate
                 frappe.db.get_value("Account", {"company": frm.doc.company, "custom_tax_code": tax_template.code}, "name")
                 .then(r => {
@@ -79,33 +102,69 @@ frappe.ui.form.on("Tax Branch Configurations", {
             // Refresh the field to reflect changes
             frm.refresh_field("item_tax_configuration");
         }
-    }
+    },
+    create_taxes(frm) {
+        if(!frm.doc.__islocal){
+            frappe.confirm('This action will disable any other default tax templates for sales and purchase, proceed?',
+                () => {
+                    frappe.call({
+                        method: "create_item_tax_templates",
+                        doc: frm.doc,
+                        args: {
+                            message:{
+                                data:{
+                                    taxes: frm.doc.item_tax_configuration
+                                }
+                            }
+                        },
+                        callback: function (r) {
+                            if(r.message){
+                                if(r.message.status == "Success"){
+                                    frm.refresh_field("item_tax_configuration")
+                                }
+                                frm.save()
+                            }
+                        }
+                    })
+                }, () => {
+                    frappe.msgprint("Action rejected by user!")
+            })
+        }else{
+            frappe.msgprint("You need to save the document first!")
+        }
+        
+	}
 });
 
 const accounts = [
     {
         "account_name": "VAT-A-Exempt",
         "tax_code": "A",
+        "account_type": "Tax",
         "tax_rate": 0
     },
     {
         "account_name": "VAT-B-16.00%",
         "tax_code": "B",
+        "account_type": "Tax",
         "tax_rate": 16
     },
     {
         "account_name": "VAT-C-0%",
         "tax_code": "C",
+        "account_type": "Tax",
         "tax_rate": 0
     },
     {
         "account_name": "VAT-D-Non-VAT",
         "tax_code": "D",
+        "account_type": "Tax",
         "tax_rate": 0
     },
     {
         "account_name": "VAT-E-8%",
         "tax_code": "E",
+        "account_type": "Tax",
         "tax_rate": 8
     }
 ]
--- kenya_etims_compliance/kenya_etims_compliance/doctype/tax_branch_configurations/tax_branch_configurations.py
+++ kenya_etims_compliance/kenya_etims_compliance/doctype/tax_branch_configurations/tax_branch_configurations.py
@@ -27,6 +27,9 @@ class TaxBranchConfigurations(Document):
                     new_acc = frappe.new_doc("Account")
                     new_acc.account_name = item.get("account_name")
                     new_acc.company = self.company
+                    new_acc.account_type = item.get("account_type")
+                    new_acc.tax_rate = item.get("tax_rate")
+                    new_acc.custom_tax_code = item.get("tax_code")
                     new_acc.parent_account = item.get("parent_account")
                     
                     new_acc.insert()
@@ -38,7 +41,44 @@ class TaxBranchConfigurations(Document):
                     
             else:
                 frappe.msgprint(f'Account {item.get("account_name")} exists for company {self.company}.')
-                
+    
+    @frappe.whitelist()
+    def create_item_tax_templates(self):
+        if not frappe.form_dict.message:
+            frappe.throw("Missing 'message' in request data")
+
+        raw_data = json.loads(frappe.form_dict.message)        
+        data = raw_data.get("data")
+
+        if not data or not data.get("taxes"):
+            frappe.throw("Missing 'item_tax_template' field in the request data")
+        
+        for item in data.get("taxes"):
+            itax_exists = frappe.db.exists("Item Tax Template", {"title": item.get("title"), "company": self.company})
+            
+            if not itax_exists:
+                try:
+                    new_itt = frappe.new_doc("Item Tax Template")
+                    new_itt.title = item.get("title")
+                    new_itt.company = self.company
+                    new_itt.custom_code = item.get("code")
+                    new_itt.custom_code_name = item.get("code_name")
+                    
+                    new_itt.append("taxes",{
+                        "tax_type":item.get("account"),
+                        "tax_rate":item.get("tax_rate")
+                    })
+                    
+                    new_itt.insert()
+                    frappe.db.commit()
+                    
+                    frappe.msgprint(f'Item Tax Template {new_itt.name} has been created.')
+                except:
+                    frappe.throw(f'Unexpected error with account {item.get("title")}!')
+                    
+            else:
+                frappe.msgprint(f'Item Tax Template {item.get("title")} exists for company {self.company}.')
+    
 def disable_default_taxes(company):
     disable_sales_taxes(company)
     disable_purchase_taxes(company)
